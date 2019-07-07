497af87cd2b0e7b4030f6c5ed0b210d6ba6455b4
Carrosen <carla1.rosen@gmail.com>
2019-07-07 16:05:04 +0200

    Adds links to other components in Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Header extends Component {
   
   render() {
     return (
       <>
+        <ul>
+          <li>
+            <Link to='/'>portfolio</Link>
+          </li>
+          <li>
+            <Link to='about'>About</Link>
+          </li>
+          <li>
+            <Link to='projects'>Projects</Link>
+          </li>
+        </ul>
       </>
     )
   }
